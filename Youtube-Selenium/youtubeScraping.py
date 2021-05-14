94887568257b90e694483dfb1691b88206433893
AlejandroPizano <alex.pizano1234@gmail.com>
2021-05-14 11:15:03 -0600

    Scraping youtube with selenium

--- Youtube-Selenium/youtubeScraping.py
+++ Youtube-Selenium/youtubeScraping.py
@@ -10,7 +10,8 @@ videos = driver.find_elements_by_class_name('style-scope ytd-rich-grid-media')
 
 for video in videos:
     name = video.find_element_by_xpath('.//*[@id="meta"]/h3').text
-    views =video.find_element_by_xpath('.//*[@id="metadata-line"]/span[1]').text.replace("views", "reproducciones")
-    who =video.find_element_by_xpath('.//*[@id="text"]/a').text
+    views = video.find_element_by_xpath('.//*[@id="metadata-line"]/span[1]').text.replace("views", "reproducciones")
+    who = video.find_element_by_xpath('.//*[@id="text"]/a').text
+    link = video.find_element_by_xpath('.//*[@id="video-title-link"]').get_attribute('href')
     if name:
-        print("Video: "+name, views+". Author :", who)
+        print("Video: "+name, views+". Author :"+ who+". Link:  "+link )
