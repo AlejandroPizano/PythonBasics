c3c812e41ca739cd1a81a28a4ceeb491cbc28f6e
AlejandroPizano <alex.pizano1234@gmail.com>
2021-05-18 09:20:34 -0600

    paginacion funcionando

--- Youtube-Selenium/coopeljs.py
+++ Youtube-Selenium/coopeljs.py
@@ -1,18 +1,27 @@
 from requests_html import HTMLSession
 import pandas as pd
-lista=[]
-url='https://www.coppel.com/refrigeradores-y-congeladores'
-def page(url):
-    s= HTMLSession()
+lista = []
+#url='https://www.coppel.com/refrigeradores-y-congeladores'
+s= HTMLSession()
+def request (url):
     res = s.get(url)
     res.html.render(sleep=1)
 
     print(res.status_code)
-    products = res.html.xpath('//*[@id="searchBasedNavigation_widget_6_2303"]/div[1]/div[2]', first= True)
+    return res.html.xpath('//*[@id="searchBasedNavigation_widget_6_2303"]/div[1]/div[2]', first= True)
+
+
+def parse(products):
     for product in products.absolute_links:
-        res= s.get(product)
-        name = res.html.find('div.top.namePartPriceContainer.clearfix', first= True).text
-        price = res.html.find('div.pcontado', first=True).text.replace("de contado","")
+        try:
+            res= s.get(product)
+            name = res.html.find('div.top.namePartPriceContainer.clearfix', first= True).text
+            price = res.html.find('div.pcontado', first=True).text.replace("de contado","")
+            flag=True
+        except:
+            print("No se encontro el producto!!")
+            flag=False
+
         if res.html.find('div.p_oferta'):
             price = res.html.find('div.tam_normal', first=True).text.replace("de contado","").replace('&nbsp;', '')
             oferta ="Producto en oferta"
@@ -21,19 +30,28 @@ def page(url):
             oferta = "Precio regular"
         datos={
             'Nombre': name,
-            'Precio':price,
-            'Promo':oferta,
+            'Precio': price,
+            'Promo': oferta,
             'link': product,
         }
         lista.append(datos)
-acum = -12
-if (acum<156):
-    acum=acum+12
-    page(f"https://www.coppel.com/refrigeradores-y-congeladores/#facet:&productBeginIndex:{acum}&orderBy:&pageView:list&minPrice:&maxPrice:&pageSize:&")
-page(url)
-df = pd.DataFrame(lista)
-df.to_csv("coppel.csv")
 
 
+def output():
+    df = pd.DataFrame(lista)
+    df.to_csv("coppel.csv")
+
+
+x=0
+while x<48 and x!= 48:
+    print("pagina :" + str(x))
+    products= request(f'https://www.coppel.com/refrigeradores-y-congeladores/#facet:&productBeginIndex:{x}&orderBy:&pageView:list&minPrice:&maxPrice:&pageSize:&')
+    parse(products)
+    x=x+12
+else:
+    print("No more items!")
+
+
+output()
 #https://www.coppel.com/refrigeradores-y-congeladores/#facet:&productBeginIndex:12&orderBy:&pageView:list&minPrice:&maxPrice:&pageSize:&
 #https://www.coppel.com/refrigeradores-y-congeladores/#facet:&productBeginIndex:24&orderBy:&pageView:list&minPrice:&maxPrice:&pageSize:&
